19053667c9d3bc831f003cd15d100ff0cfe530dc
denven <myregs@foxmail.com>
2019-10-23 13:54:47 -0700

    test basic user input command

--- client.js
+++ client.js
@@ -5,9 +5,20 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.88.149',
+    //host: '192.168.88.149',
+    host: '127.0.0.1',
     port: 50541
   });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server.");
+    conn.write("Name: ABC");
+    conn.write("Move: up");
+    //conn.write("Move: down");
+    //conn.write("Move: left");
+    //conn.write("Move: right");
+  });
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on("data", (data) => {
